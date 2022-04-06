62ece298ee50809192c20dc67b1f5dca2be5d6a0
RaphaelPittet <pittetr.96@gmail.com>
2022-04-06 20:10:23 +0200

    smart contract updated to store message with the wave, and for luck guy return a little amount of ETH when he waves.

---
+++ address.md
@@ -0,0 +1,4 @@
+## Smart Contract address 
+
+## wave Portal testnet Rinkeby
+0x8193943C35c53FbA3209C218A93Bfa5F0Bb1CA9f
--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,17 +7,44 @@ import "hardhat/console.sol";
 contract WavePortal {
 
     uint256 totalWaves;
+    uint256 private seed;
+    // mapping of all addres who waved me and the number of wave 
     mapping(address => uint) public senderCount;
+    // array used as an index for senderCount mapping
     address[] public addressIndices;
 
-    constructor() {
-        console.log("Yo yo, I am your first smart contract !");
+    // storing the address with the last time the user waved at us.
+    mapping(address => uint256) public lastWavedAt;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+    /*
+     * struct named Wave.
+     */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
     }
+
+    /*
+     * Declaration of a variable waves that lets us store an array of structs.
+     */
+    Wave[] waves;
+
+
+    constructor() payable {
+        console.log("Yo yo, I am your first smart contract !");
+        //Set the initial seed
+        seed = (block.timestamp + block.difficulty) % 100;
+
+    }
+
 
     // get number of wave done by userAdress
     function currentCount(address userAddress) public view returns (uint) {
      return senderCount[userAddress];
     }
+
 
     // get address who done the most wave
     function getTopWaverAddress() public view returns (address){
@@ -34,6 +61,7 @@ contract WavePortal {
         return bestAddress;
     }
 
+
     // get number of wave done by the most waved address
     function getTopWaverCount() public view returns (uint){
         address topWaverAddress = getTopWaverAddress();
@@ -42,21 +70,61 @@ contract WavePortal {
         return senderCount[topWaverAddress];
     }
 
-    function wave() public {
+
+    function wave(string memory _message) public {
+
+        // Make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+        // Update the current timestamp we have for the user
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
-        uint256 isExist;
+
+        bool isExist;
         senderCount[msg.sender] += 1;
         for (uint256 i = 0; i < addressIndices.length; i++) {
             if(msg.sender == addressIndices[i]){
-                isExist =1;
+                isExist =true;
                 break;
             }
         }
-        if(isExist != 1){
+        if(isExist != true){
             addressIndices.push(msg.sender);
         }
 
         console.log("%s has waved!", msg.sender);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        
+        // Generate a new seed for the next user that sends a wave
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %d", seed);
+
+
+        //Give a 20% chance that the user wins the prize.
+        if (seed <= 30) {
+            console.log("%s won!", msg.sender);
+
+            // functionality to send money to waver
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+            );
+
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    /*
+     * Return the struct array, waves, to us.
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256){
